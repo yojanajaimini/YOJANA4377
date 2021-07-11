f63e1f000908a3bbf50b18e34d11e8d83bcb5f11
yojanajaimini <84927164+yojanajaimini@users.noreply.github.com>
2021-07-11 20:43:57 +0530

    Add files via upload

---
+++ TIMERBASE_ALARM.ino
@@ -0,0 +1,15 @@
+int led =4;
+void setup() {
+  pinMode(led,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(led,HIGH);
+  delay(2000);
+  digitalWrite(led,LOW);
+  delay(15000);
+  // put your main code here, to run repeatedly:
+
+}
