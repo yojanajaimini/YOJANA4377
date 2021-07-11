a489dabe63594de8e3f22c4ee1cb74afa188e620
yojanajaimini <84927164+yojanajaimini@users.noreply.github.com>
2021-07-11 20:46:22 +0530

    Add files via upload

---
+++ sa_re_ga_ma.ino
@@ -0,0 +1,87 @@
+// C++ code
+//
+int buzzer=1;
+void setup()
+{
+  pinMode(1,OUTPUT);
+}
+
+void loop()
+{
+  tone(1,240);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,270);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,300);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,320);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,360);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,400);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,450);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,480);
+  delay(500);
+  noTone(1);
+  delay(500);
+  
+}// C++ code
+//
+int buzzer=1;
+void setup()
+{
+  pinMode(1,OUTPUT);
+}
+
+void loop()
+{
+  tone(1,240);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,270);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,300);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,320);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,360);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,400);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,450);
+  delay(500);
+  noTone(1);
+  delay(500);
+  tone(1,480);
+  delay(500);
+  noTone(1);
+  delay(500);
+  
+}
